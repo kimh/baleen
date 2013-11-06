c277258043360c1734ef9577ef92365191737177
Kim, Hirokuni <hirokuni.kim@kvh.co.jp>
2013-11-06 23:14:03 +0900

    some change

--- spec/lib/baleen/spec_result_display.rb
+++ spec/lib/baleen/spec_result_display.rb
@@ -22,5 +22,26 @@ describe Baleen::ResultDisplay do
         capture(:stdout) { displayer.summary }.should include 'Pass'
       end
     end
+
+    context "when some test fails" do
+      it "should display Fail" do
+         result = [
+          {
+            "status_code" => 0,
+            "container_id" => "aaaaaa",
+            "log" => "brabrabra",
+            "file" => "feature/t1.feature"
+          },
+          {
+            "status_code" => 1,
+            "container_id" => "bbbbbb",
+            "log" => "brabrabra",
+            "file" => "feature/t2.feature"
+          }
+        ]
+        displayer = Baleen::ResultDisplay.new(result, Time.now, Time.now+10)
+        capture(:stdout) { displayer.summary }.should include 'Fail'
+      end
+    end
   end
 end
